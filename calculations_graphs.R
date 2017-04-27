93614d118e97ec1c3a3eceff31684835b454ac9b
Adam Jenkins <adammjenks@gmail.com>
2017-04-27 08:10:16 -0400

    Pipes and utility data

--- calculations_graphs.R
+++ calculations_graphs.R
@@ -23,9 +23,19 @@ Energy_Parsed_Df$`Total Site Energy (kBTU)` <- as.numeric(Energy_Parsed_Df$`Tota
  
 Energy_Parsed_Df <-  Energy_Parsed_Df %>%
                       mutate(Available_sqft_for_panels = sqft_available * 0.66,
-                             Number_of_300watt_Panels = Available_sqft_for_panels/ 20.67,
+                             Number_of_300watt_Panels = Available_sqft_for_panels / 20.67,
                              Kwh_potential = Number_of_300watt_Panels * 300 * sunlight_hours * 0.75,
                              Total_Site_Energy_Kwh = (`Total Site Energy (kBTU)` * 1000) * 0.00029307107017,
                              Total_Site_Energy_Kwh_Electricity = Total_Site_Energy_Kwh * `% Electricity`,
-                             Cost_of_installation_gross = Number_of_300watt_Panels * 300 * 4.20)
+                             Cost_of_installation_gross = Number_of_300watt_Panels * 300 * 4.20,
+                             Surplus_energy_production_possible = Kwh_potential - Total_Site_Energy_Kwh_Electricity)
 
+Energy_Surplus_per_property_type <- Energy_Parsed_Df %>% 
+                                        filter(!is.na(Surplus_energy_production_possible)) %>%
+                                        group_by(`Property Type`) %>%
+                                        summarise(Surplus_energy_production_possible = sum(Kwh_potential - Total_Site_Energy_Kwh_Electricity),
+                                                  Total_cost = sum(Cost_of_installation_gross)) %>%
+                                        mutate(Exceeds_all_of_boston_by = Surplus_energy_production_possible - sum(Energy_Parsed_Df$Total_Site_Energy_Kwh_Electricity, na.rm = T))
+
+
+Utility_data_raw <- read.csv('data/city-of-boston-utility-data-2011-2015.csv')
