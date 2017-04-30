7d3f112f3e43ddbc62e5455db7d61845769a0be1
Adam Jenkins <adammjenks@gmail.com>
2017-04-30 10:10:32 -0400

    Rearrangement and addition of tabs
    Reformatted how things were arranged

--- ui.R
+++ ui.R
@@ -8,8 +8,8 @@ dashboardPage(skin = "green",
   dashboardSidebar(
     sidebarMenu(
       menuItem("Home", tabName = "dashboard", icon = icon("home")),
-      menuItem("Maps", tabName = "maps", icon = icon("map")),
-      menuItem("Energy", tabName = "energy", icon = icon("bolt")),
+      menuItem("Energy Skyline", tabName = "maps", icon = icon("map")),
+      menuItem("Boston Explorer", tabName = "energy", icon = icon("bolt")),
       menuItem("Scenario Planner", tabName = "scenario", icon = icon("money"))
     )
   ),
@@ -89,11 +89,10 @@ dashboardPage(skin = "green",
               fluidRow(
                 box (title = 'Maps', width = 12, class="mapbox", status = 'success', solidHeader = TRUE,
                   div(id = "map")
-                ),
+                )
                 #box (title = 'Maps', width = 12, status = 'success', solidHeader = TRUE
                   #tags$iframe(src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d94411.77160300482!2d-71.1273685766368!3d42.31335203482073!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e3652d0d3d311b%3A0x787cbf240162e8a0!2sBoston%2C+MA!5e0!3m2!1sen!2sus!4v1492800915327", width="100%", height="800", frameborder="0", style="border:0")    
                 #),
-                leafletOutput("mymap")
               )
       ),
       tabItem(tabName = "energy",
@@ -102,7 +101,11 @@ dashboardPage(skin = "green",
                 box(title = 'Energy', width = 12, status = 'success', solidHeader = TRUE,
                   selectInput('type', label="Property Type", choices = c('All', sort(unique(Energy_Parsed_Df$`Property Type`)))),
                   numericInput('year', label="Year Built >=", min=min(Energy_Parsed_Df$year_built), max=max(Energy_Parsed_Df$year_built), value = 1900),
-                  DT::dataTableOutput('energy_table')
+                  
+                  tabsetPanel(
+                    tabPanel('Table', DT::dataTableOutput('energy_table')),
+                    tabPanel('City Map',leafletOutput("mymap"))
+                  )
                 )
               )
       ),
@@ -111,10 +114,16 @@ dashboardPage(skin = "green",
         fluidRow(
           box(
               title = 'Input Values For Scenario', width = 12, status = 'success', solidHeader = TRUE,
-              dropdownButton(
-                label = "Choose Property Types", status = "default", width = 460,
-                checkboxGroupInput(inputId = "prop_types", label = "Property Types", choices = unique(Energy_Parsed_Df$`Property Type`))
-                )
+              column(width = 6,
+                  dropdownButton(
+                    label = "Choose Property Types", status = "default", width = 460,
+                    checkboxGroupInput(inputId = "prop_types", label = "Property Types", choices = unique(Energy_Parsed_Df$`Property Type`))
+                    ),
+                  HTML("<br>"),
+                  numericInput(inputId = "city_bost_perc", label = "Percentage of Electricity to Cover", 
+                               min = 0, max = 100, value = 50, width = 250)
+              ),
+              column(width = 6)
             )
           )
         )
